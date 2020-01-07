e9150e5f58a38c1dcb7bf6a4ef75141bc15796ba
hvl1099 <hvl1099@gepplus.co.kr>
2020-01-07 14:45:20 +0900

    Updates

---
+++ precache-manifest.859054219d0b71227909ce62b30b2bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3486ab8bb42f89bb9584",
+    "url": "/react-coin/static/js/runtime~main.3486ab8b.js"
+  },
+  {
+    "revision": "29dd31b6b809f9ec6479",
+    "url": "/react-coin/static/js/main.29dd31b6.chunk.js"
+  },
+  {
+    "revision": "148b4577ebaefb660986",
+    "url": "/react-coin/static/js/2.148b4577.chunk.js"
+  },
+  {
+    "revision": "29dd31b6b809f9ec6479",
+    "url": "/react-coin/static/css/main.19188955.chunk.css"
+  },
+  {
+    "revision": "148b4577ebaefb660986",
+    "url": "/react-coin/static/css/2.8548541f.chunk.css"
+  },
+  {
+    "revision": "fc783c0e109060af4374098ef7390b0f",
+    "url": "/react-coin/index.html"
+  }
+];
